e42c2f0bab5416980b5fe11d678c277d23c378e1
Maarit Laakso <maarit.laakso2@myy.haaga-helia.fi>
2022-03-23 19:24:23 +0200

    staticfiles missing

--- example_project/settings.py
+++ example_project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
- 
+    'django.contrib.staticfiles',
     'example_project.app1',
     'example_project.app2' 
 ]
