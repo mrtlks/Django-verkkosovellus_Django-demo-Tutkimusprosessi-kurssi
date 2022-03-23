314557ff361b9c3c80690ccc4e71f1de2540308a
Maarit Laakso <maarit.laakso2@myy.haaga-helia.fi>
2022-03-23 20:13:38 +0200

    collect static

--- example_project/settings.py
+++ example_project/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -28,14 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Application definition
 
